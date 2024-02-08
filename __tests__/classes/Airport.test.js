a1c8bc953ff5c787f9f0d526d6c38aa90bea9c41
Zachary Harris <the1zachharris@gmail.com>
2024-02-08 12:58:07 -0600

    person class with tests

--- __tests__/classes/Airport.test.js
+++ __tests__/classes/Airport.test.js
@@ -1,19 +1,19 @@
 const Airport = require('../../classes/Airport.js')
 
 describe('Airport', () => {
-    let airport;
+    let airport
 
     beforeEach(() => {
-        airport = new Airport('Test Airport', 'TST');
-    });
+        airport = new Airport('Test Airport', 'TST')
+    })
 
     it('should initialize with an empty array of planes', () => {
-        expect(airport.getPlanes()).toEqual([]);
-    });
+        expect(airport.getPlanes()).toEqual([])
+    })
 
     it('should add a plane to the airport', () => {
-        const plane = { id: 1, name: 'Test Plane' };
-        airport.addPlane(plane);
-        expect(airport.getPlanes()).toContain(plane);
-    });
-});
+        const plane = { id: 1, name: 'Test Plane' }
+        airport.addPlane(plane)
+        expect(airport.getPlanes()).toContain(plane)
+    })
+})
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Import classes here
-
+const Airport = require('../../classes/Airport.js')
 
 function main() {
     // You can run any of your classes here to test the outputs
