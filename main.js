888e55596c21e3a473ed0a11e6e6b559afcd842c
Zachary Harris <the1zachharris@gmail.com>
2024-02-08 14:40:28 -0600

    added imports to main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Import classes here
 
 const Airport = require('../../classes/Airport.js')
+const Airport = require('../../classes/Bag.js')
+const Airport = require('../../classes/Person.js')
+const Airport = require('../../classes/Plane.js')
 
 function main() {
     // You can run any of your classes here to test the outputs
